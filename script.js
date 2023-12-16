870472d7093e27ab6c6856ae86edfd21eb1181cf
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-16 20:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+function mapLetters(word) {
+
+	const mapLetters = {};
+
+
+
+
+	for (let i = 0; i < word.length; i++) {
+
+		const letter = word[i];
+
+
+
+
+		if (mapLetters.hasOwnProperty(letter)) {
+
+			mapLetters[letter].push(i);
+
+		}else{
+
+			mapLetters[letter] = [i];
+
+		}
+
+	}
+
+
+
+
+	return mapLetters;
+
+}
+
+
+
+
+console.log(mapLetters("dodo")); 
+
+console.log(mapLetters("froggy")); 
+
+console.log(mapLetters("grapes"))
